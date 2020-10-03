78c36db8c99ee96973586af9e9132e6f9cffd74b
HariKotha <harimsd8@gmail.com>
2020-10-03 20:19:56 +0530

    Added Products Screen 📱⌚

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
-        <Container>
-          <HomeScreen />
-        </Container>
+        <Switch>
+          <Container>
+            <Route path="/" exact component={HomeScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+          </Container>
+        </Switch>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Rating from '../components/Rating';
+import products from '../products';
+
+const ProductScreen = ({ match }) => {
+  const product = products.find(p => p._id === match.params.id);
+
+  return (
+    <>
+      <Link to="/" className="btn btn-light btn-sm my-3 rounded">
+        <i className="fas fa-chevron-left mr-2"></i>Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} thumbnail fluid />
+        </Col>
+        <Col md={3} className="my-2">
+          <ListGroup>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating rating={product.rating} reviews={product.numReviews} />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3} className="my-2">
+          <Card>
+            <ListGroup>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    <Button className="btn btn-block rounded" disabled={product.countInStock <= 0}>
+                      Add to Cart
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
